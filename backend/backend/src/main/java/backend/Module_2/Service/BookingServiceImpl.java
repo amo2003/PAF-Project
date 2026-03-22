e003886ac5eaab681f02ef5a127518c07738b55b
Amod Indupa <amodindupa9@gmail.com>
2026-03-22 19:38:20 +0530

    Update BookingServiceImpl.java

--- backend/backend/src/main/java/backend/Module_2/Service/BookingServiceImpl.java
+++ backend/backend/src/main/java/backend/Module_2/Service/BookingServiceImpl.java
@@ -4,6 +4,8 @@ import backend.Module_2.Model.Booking;
 import backend.Module_2.Repository.BookingRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookingServiceImpl {
     private final BookingRepository bookingRepository;
@@ -28,5 +30,40 @@ public class BookingServiceImpl {
         );
     }
 
-    
+    @Override
+    public BookingResponse createBooking(BookingRequest request) {
+        //check sheduling conflict
+        List<Booking> conflicts bookingRepository.findConflictingBookings(
+                request.getResourceId(),
+                request.getBookingDate(),
+                request.getStartTime(),
+                request.getEndTime()
+        );
+
+        if (!conflicts.isEmpty()) {
+            throw new RuntimeException("Booking already exists");
+        }
+
+        Booking booking = new Booking(
+                request.getUserId(),
+                request.getResourceId(),
+                request.getBookingDate(),
+                request.getStartTime(),
+                request.getEndTime(),
+                request.getPurpose(),
+                request.getAttendees(),
+                BookingStatus.PENDING
+        );
+
+        return toReponse(bookingRepository.save(booking));
+    }
+
+    @Override
+    public BookingResponse getBookingById(Long id) {
+        Booking booking = bookingRepository.findBy(id)
+                .orElseThrow(() -> new RuntimeException("Booking not found with id: " + id));
+        return toReponse(booking);
+    }
+
+    @Override
 }
