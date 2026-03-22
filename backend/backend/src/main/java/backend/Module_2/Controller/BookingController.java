112ba7509216fe7deab65683896b2b1a61dfab7d
Amod Indupa <amodindupa9@gmail.com>
2026-03-22 20:15:34 +0530

    Create BookingController.java

---
+++ backend/backend/src/main/java/backend/Module_2/Controller/BookingController.java
@@ -0,0 +1,26 @@
+package backend.Module_2.Controller;
+
+
+import backend.Module_2.Service.BookingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bookings")
+@CrossOrigin("http://localhost:3000")
+public class BookingController {
+
+    public final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    // POST /api/bookings — User creates a booking request
+    @PostMapping("/{id}")
+    public ResponseEntity<BookingResponse> createBooking(@Valid @RequestBody BookingRequest request) {
+        BookingResponse response = bookingService.createBooking(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
