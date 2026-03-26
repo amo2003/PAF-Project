54ce7619cc2efb4bdc88b3cc4c4af3f547a1905d
Amod Indupa <amodindupa9@gmail.com>
2026-03-26 21:20:53 +0530

    bookingCrud update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './Module2/Components/Navbar';
-import Home from './Home/Home';
-import CreateBooking from './Module2/Pages/CreateBooking';
-import MyBookings from './Module2/Pages/MyBooking';
-import AdminBookings from './Module2/Pages/AdminBookings';
+import { Routes } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/"                element={<Home />} />
-        <Route path="/create-booking"  element={<CreateBooking />} />
-        <Route path="/my-bookings"     element={<MyBookings />} />
-        <Route path="/admin/bookings"  element={<AdminBookings />} />
         
       </Routes>
     </>
