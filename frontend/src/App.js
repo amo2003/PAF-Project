743191d15304d25783b92fff496d7d07be8f436e
Amod Indupa <amodindupa9@gmail.com>
2026-03-26 19:32:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Home from './Home/Home';
 import CreateBooking from './Module2/Pages/CreateBooking';
 import MyBookings from './Module2/Pages/MyBooking';
 import AdminBookings from './Module2/Pages/AdminBookings';
+import AdminResources from './pages/AdminResources';
+import ChatBot from './Module2/Pages/ChatBot';
 import './App.css';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Route path="/my-bookings"     element={<MyBookings />} />
         <Route path="/admin/bookings"  element={<AdminBookings />} />
         
+        <Route path="/admin/resources" element={<AdminResources />} />
+        <Route path="/chat"            element={<ChatBot />} />
       </Routes>
     </>
   );
