66280256cf8007bb1db0da90b76cbae409ca6a76
Amod Indupa <amodindupa9@gmail.com>
2026-03-29 02:38:03 +0530

    update frontend pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import './index.css'
 import App from './App.jsx'
 
@@ -11,6 +10,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </StrictMode>
-    <App />
-  </StrictMode>,
 )
